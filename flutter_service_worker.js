4faf782d43b86c3bb833ee863e062a43a7f8b1e4
mudassir-moin <mm.mudassirmoin@gmail.com>
2026-05-15 15:04:35 +0000

    Deploying to gh-pages from @ mudassir-moin/bear-summit@93ea96a558cc80a75d096bcec14725974b16caca 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/canvaskit.js": "738255d00768497e86aa4ca510cce1e1",
 "canvaskit/skwasm.wasm": "4051bfc27ba29bf420d17aa0c3a98bce",
-"flutter_bootstrap.js": "b51f7360bb4cdd7bcd6b8aaff2ecb340",
+"flutter_bootstrap.js": "d1fc43376d45315df5bcb289cfc20894",
 "manifest.json": "97bfa7b9ca6995c567c2cca621ea4496",
 "main.dart.js": "93a21f6143718c7bc849b45093758cc5"};
 // The application shell files that are downloaded before a service worker can
